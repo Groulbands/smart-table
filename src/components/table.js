01ebdecc06f6ef749d5a7fc6c7d1cfbcc1594a64
Groulbands <groulbands@gmail.com>
2026-01-18 15:45:45 +0300

    fix issues after 1st revue

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,41 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(itemTemplate => { 
+        root[itemTemplate] = cloneTemplate(itemTemplate); 
+        root.container.prepend(root[itemTemplate].container); 
+    }) 
+    after.forEach(itemTemplate => { 
+        root[itemTemplate] = cloneTemplate(itemTemplate); 
+        root.container.append(root[itemTemplate].container); 
+    }) 
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
+    root.container.addEventListener('change', (e) => { 
+        onAction() 
+    }) 
+    root.container.addEventListener('reset', (e) => { 
+        setTimeout(() => onAction()) 
+    }) 
+    root.container.addEventListener('submit', (e) => { 
+        e.preventDefault() 
+        onAction(e.submitter) 
+    })
+
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (row.elements[key] && ['INPUT', 'SELECT'].includes(row.elements[key].tagName)) { 
+                    row.elements[key].value = item[key]; 
+                } else if (row.elements[key]) { 
+                    row.elements[key].textContent = item[key]; 
+                }
+            });
+            return row.container;
+        })
         root.elements.rows.replaceChildren(...nextRows);
     }
 
